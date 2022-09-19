80a7377d54bb414fc509b70ccc3cbe503eab74b2
Rodrigo Prado de Jesus <royopa@gmail.com>
2022-09-19 20:45:34 -0300

    adiciona latitude e longitude

--- app/src/main/java/com/herokuapp/infovacinas/SecondFragment.kt
+++ app/src/main/java/com/herokuapp/infovacinas/SecondFragment.kt
@@ -1,12 +1,19 @@
 package com.herokuapp.infovacinas
 
+import android.R
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.herokuapp.infovacinas.databinding.FragmentSecondBinding
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,6 +21,9 @@ import com.herokuapp.infovacinas.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private var latitude: TextView? = null
+    private var longitude: TextView? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,8 +35,42 @@ class SecondFragment : Fragment() {
     ): View? {
 
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        latitude = _binding.latitude
+        longitude = _binding.longitude
+        val button = _binding.btnBuscarPorCoordenada
+
+        button.setOnClickListener() {
+            getLocation()
+        }
+
         return binding.root
 
+    }
+
+    private fun getLocation()
+    {
+        //check location permission
+        if ((ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) &&
+                (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED)) {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 100)
+                return
+        }
+
+        // pega a latitude e a longitude
+        val location = fusedLocationProviderClient.lastLocation
+        location.addOnSuccessListener {
+            if(it!=null) {
+                var textLatitude = "Latitude: "+it.latitude.toString()
+                var textLongitude = "Longitude: "+it.longitude.toString()
+                latitude.text = textLatitude
+                longitude.text = textLongitude
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
